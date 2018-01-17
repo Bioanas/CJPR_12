d8d9c92bb3a056f71e8857b78896e2cf4dd3fc80
Bioanas <33870208+Bioanas@users.noreply.github.com>
2018-01-17 10:22:23 +0200

    Initial commit

---
+++ src/com/ia/Main.java
@@ -0,0 +1,18 @@
+package com.ia;
+
+public class Main {
+//exercitiul1
+    public static void main(String[] args) {
+
+    }
+    abstract class Animal {
+
+    }
+    class Dog extends Animal {
+    Dog dog = new Dog ();
+    }
+
+    class Horse extends Animal{
+     Horse horse = new Horse ();
+    }
+}
